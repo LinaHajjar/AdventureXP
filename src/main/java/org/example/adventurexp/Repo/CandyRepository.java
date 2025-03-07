de72987d1db39e264401caf792cfa14f1c7ecebf
YousafTJ <youjav@hotmail.com>
2025-03-07 15:21:19 +0100

    Instructor and shift og slik

--- src/main/java/org/example/adventurexp/Repo/CandyRepository.java
+++ src/main/java/org/example/adventurexp/Repo/CandyRepository.java
@@ -3,5 +3,9 @@ package org.example.adventurexp.Repo;
 import org.example.adventurexp.Model.Candy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CandyRepository extends JpaRepository<Candy, Integer> {
+    Optional<Candy> findByName(String name);
+
 }
