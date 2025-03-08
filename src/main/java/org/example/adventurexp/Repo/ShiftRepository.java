2ce122c4df2bf07e72434dbe8b414fe664b5ac28
Lina Al-Hajjar <lial0001@stud.kea.dk>
2025-03-08 13:59:12 +0100

    refactoring

--- src/main/java/org/example/adventurexp/Repo/ShiftRepository.java
+++ src/main/java/org/example/adventurexp/Repo/ShiftRepository.java
@@ -3,8 +3,11 @@ package org.example.adventurexp.Repo;
 import org.example.adventurexp.Model.Instructor;
 import org.example.adventurexp.Model.Shift;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface ShiftRepository extends JpaRepository<Shift, Integer> {
 
     List<Shift> findByInstructor(Instructor instructor);
